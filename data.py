de2b3190e390da980b4b25f2dd7c44e9fd2ba598
Lucas Willems <lcswillems@gmail.com>
2018-11-17 23:55:27 +0100

    Better data augmentation

--- data.py
+++ data.py
@@ -3,21 +3,23 @@ import os
 
 import torchvision.transforms as transforms
 
-size = 224
+size = 256
+crop_size = 224
 
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
 
 train_data_transforms = transforms.Compose([
-    transforms.RandomResizedCrop(size),
+    transforms.Resize((size, size)),
+    transforms.RandomCrop(crop_size),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     normalize
 ])
 
 val_data_transforms = transforms.Compose([
-    transforms.Resize(int(size*1.14)),
-    transforms.CenterCrop(size),
+    transforms.Resize((size, size)),
+    transforms.CenterCrop(crop_size),
     transforms.ToTensor(),
     normalize
 ])
