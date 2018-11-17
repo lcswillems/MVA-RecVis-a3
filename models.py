f8499c24f4def5d7e5efd4087d69a15ed2f300df
Lucas Willems <lcswillems@gmail.com>
2018-11-17 11:50:12 +0100

    Several models

---
+++ models.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+
+nclasses = 20
+
+class Baseline(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv3 = nn.Conv2d(20, 20, kernel_size=5)
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, nclasses)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = F.relu(F.max_pool2d(self.conv3(x), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+class Based(nn.Module):
+    def __init__(self, base):
+        super().__init__()
+
+        self.base = getattr(models, base)(pretrained=True)
+        self.base.fc = nn.Linear(self.base.fc.in_features, nclasses)
+
+        for weights in self.base.parameters():
+            weights.requires_grad = False
+        for weights in self.base.layer4[-1].conv2.parameters():
+            weights.requires_grad = True
+
+    def forward(self, x):
+        return self.base(x)
+
+class Resnet18(Based):
+    def __init__(self):
+        super().__init__("resnet18")
+
+class Resnet34(Based):
+    def __init__(self):
+        super().__init__("resnet34")
+
+class Resnet101(Based):
+    def __init__(self):
+        super().__init__("resnet101")
