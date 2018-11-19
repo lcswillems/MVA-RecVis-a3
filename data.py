4419bece0b00c17f27ef30257c21e2d3cbbde5ee
Lucas Willems <lcswillems@gmail.com>
2018-11-19 12:21:43 +0100

    Better data augmentation ?

--- data.py
+++ data.py
@@ -9,22 +9,17 @@ normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
 
 train_data_transforms = transforms.Compose([
-    transforms.Resize((256, 256)),
-    transforms.RandomCrop(224),
+    transforms.RandomResizedCrop(224, scale=(0.6, 1)),
+    transforms.RandomHorizontalFlip(),
+    transforms.ColorJitter(.4, .4, .4),
     numpy.array,
     iaa.Sequential([
-        # iaa.Sometimes(0.5,
-        #     iaa.GaussianBlur(sigma=(0, 0.5))
-        # ),
-        # iaa.Sometimes(0.5,
-        #     iaa.AdditiveGaussianNoise(scale=0.10*255),
-        # ),
-        iaa.ContrastNormalization((0.8, 1.2)),
-        iaa.Fliplr(0.5),
+        # iaa.ContrastNormalization((0.8, 1.2)),
         iaa.Affine(
-            rotate=(-25, 25),
-            shear=(-20, 20)
-        )
+            rotate=(-45, 45),
+            shear=(-25, 25)
+        ),
+        iaa.PiecewiseAffine(scale=0.045)
     ], random_order=True).augment_image,
     numpy.array,
     transforms.ToTensor(),
