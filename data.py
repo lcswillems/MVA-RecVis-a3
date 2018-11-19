7e813b89fa92c0a9a463875fc42e36dc92192b97
Lucas Willems <lcswillems@gmail.com>
2018-11-19 03:40:55 +0100

    More data augmentation

--- data.py
+++ data.py
@@ -1,24 +1,39 @@
 import zipfile
 import os
-
 import torchvision.transforms as transforms
-
-size = 256
-crop_size = 224
+from imgaug import augmenters as iaa
+import numpy
+import torch
 
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
 
 train_data_transforms = transforms.Compose([
-    transforms.RandomResizedCrop(crop_size, scale=(.6, 1)),
-    transforms.RandomHorizontalFlip(),
-    transforms.ColorJitter(.4, .4, .4),
+    transforms.Resize((256, 256)),
+    transforms.RandomCrop(224),
+    numpy.array,
+    iaa.Sequential([
+        iaa.Fliplr(0.5),
+        iaa.Sometimes(0.5,
+            iaa.GaussianBlur(sigma=(0, 0.5))
+        ),
+        iaa.ContrastNormalization((0.75, 1.5)),
+        iaa.AdditiveGaussianNoise(scale=0.10*255),
+        iaa.Affine(
+            scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+            translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+            rotate=(-25, 25),
+            shear=(-8, 8)
+        )
+    ], random_order=True).augment_image,
+    numpy.array,
     transforms.ToTensor(),
     normalize
 ])
 
 val_data_transforms = transforms.Compose([
-    transforms.Resize((size, size)),
+    transforms.Resize((256, 256)),
+    transforms.RandomCrop(224),
     transforms.ToTensor(),
     normalize
 ])
