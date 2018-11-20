90dd6f6bb5d2704c7b6df0ba7b8202786047fc1e
Lucas Willems <lcswillems@gmail.com>
2018-11-20 23:53:59 +0100

    Undo fix

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,7 +1,7 @@
 wget https://www.di.ens.fr/willow/teaching/recvis18/assignment3/bird_dataset.zip
-unzip -qf bird_dataset.zip -d .
+unzip -qf bird_dataset.zip
 rm bird_dataset.zip
 
 wget http://host.lucaswillems.com/bird_dataset_ex.zip
-unzip -qf bird_dataset_ex.zip -d .
+unzip -qf bird_dataset_ex.zip
 rm bird_dataset_ex.zip
