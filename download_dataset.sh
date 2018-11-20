24c6c09542b6a0369ca802cf75668066b53f2902
Lucas Willems <lcswillems@gmail.com>
2018-11-20 23:47:03 +0100

    Try to fix a bug

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,7 +1,7 @@
 wget https://www.di.ens.fr/willow/teaching/recvis18/assignment3/bird_dataset.zip
-unzip -qf bird_dataset.zip
+unzip -qf bird_dataset.zip -d .
 rm bird_dataset.zip
 
 wget http://host.lucaswillems.com/bird_dataset_ex.zip
-unzip -qf bird_dataset_ex.zip
+unzip -qf bird_dataset_ex.zip -d .
 rm bird_dataset_ex.zip
