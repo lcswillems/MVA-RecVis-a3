71ca0b8ebb9561ce133305821348569a7ed96bc5
Lucas Willems <lcswillems@gmail.com>
2018-11-20 17:49:45 +0100

    More data augmentation

--- data.py
+++ data.py
@@ -11,15 +11,15 @@ normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
 
 train_data_transforms = transforms.Compose([
-    transforms.Resize(224),
+    transforms.Resize(256),
     transforms.RandomHorizontalFlip(),
     transforms.ColorJitter(.4, .4, .4),
-    # numpy.array,
-    # iaa.Affine(
-    #     rotate=(-45, 45),
-    #     shear=(-25, 25)
-    # ).augment_image,
-    # lambda img: Image.fromarray(img),
+    numpy.array,
+    iaa.Affine(
+        rotate=(-45, 45),
+        shear=(-25, 25)
+    ).augment_image,
+    lambda img: Image.fromarray(img),
     transforms.RandomCrop(224),
     transforms.ToTensor(),
     normalize
