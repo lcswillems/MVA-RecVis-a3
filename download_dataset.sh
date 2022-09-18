8c9255fa815d9b1bce3e8654431ab53bdc1c991c
Lucas Willems <lcswillems@gmail.com>
2022-09-18 13:21:13 +0200

    Remove option to download precomputed extracted birds

--- download_dataset.sh
+++
@@ -1,7 +0,0 @@
-wget https://www.di.ens.fr/willow/teaching/recvis18/assignment3/bird_dataset.zip
-unzip -q bird_dataset.zip
-rm bird_dataset.zip
-
-wget http://host.lucaswillems.com/bird_dataset_ex.zip
-unzip -q bird_dataset_ex.zip
-rm bird_dataset_ex.zip
